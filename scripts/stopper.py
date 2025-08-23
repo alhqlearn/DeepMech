2d22b7b381509ca3686f020f950bf484247490de
Manajit Das <59758360+manajit-das@users.noreply.github.com>
2025-08-23 14:18:03 +0530

    Delete scripts/stopper.py

--- scripts/stopper.py
+++
@@ -1,5 +0,0 @@
-# Moved content of stopper.py to scripts/stopper.py
-
-# Original content follows:
-
-# Add the content of stopper.py here based on the latest commit 35d7a931c3cdd8b8fb834e12a42ee5609c947181
